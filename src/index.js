4005c778ceacb68c0e7a1bb6fced80c9f6cad446
June Epstein <epstein.june@gmail.com>
2017-11-24 11:02:17 -0500

    connect youtube api to state

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
+import { API_KEY } from './config';
 import SearchBar from './components/search_bar';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => this.setState({ videos }));
+    
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
