d7b93836c621a8fdf88787131af94241557df4ba
June Epstein <epstein.june@gmail.com>
2017-11-27 12:52:49 -0500

    more tutorial

--- src/index.js
+++ src/index.js
@@ -5,20 +5,33 @@ import YTSearch from 'youtube-api-search';
 import { API_KEY } from './config';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
-    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => this.setState({ videos }));
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+     };
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
     
   }
   render(){
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          videos={this.state.videos}
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+        />
       </div>
     );
   }
